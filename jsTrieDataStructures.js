dd1637422df809746d00b26949cfd724bee9de2f
Thomas M. Maestas <thomasm1@users.noreply.github.com>
2017-10-30 10:57:40 -0600

    trie concat almost finished

--- jsTrieDataStructures.js
+++ jsTrieDataStructures.js
@@ -1,5 +1,4 @@
- // data-focused js functions, direct relevance to D3js Sankey/Hierarchical Data Structures
- // trie data = structure tmm
+// trie data = structure tmm
 let Node = function() {
 	this.keys = new Map();
 	this.end = false;
@@ -26,24 +25,35 @@ let Trie = function() {
 			return this.add(input.substr(1), node.keys.get(input[0]));
 		};
 	};
-this.isWord = function(word) {
-	let node = this.root;
-	while (word.length > 1) {
-		if (!node.keys.has(word[0])) 
-			
-			return false;
+	this.isWord = function(word) {
+		let node = this.root;
+		while (word.length > 1) {
+			if (!node.keys.has(word[0]))  
+				return false;
+			} else {
+				node = node.keys.get(word[0]);
+				word = word.substr(1);
+			};
+		};
+};	
+//
+this.print = function() {
+	let words = new Array();
+	let search = function(node, string) {
+		if (node.keys.size != 0) {
+			for (let letter of node.keys.keys()) {
+				search(node.keys.get(letter), string.concat(letter));
+			};
+			if (node.is()) {
+				words.push(string);
+			};
 		} else {
-			node = node.keys.get(word[0]);
-			word = word.substr(1);
+			string.length > 0 ? words.push(string) : undefined:
+			return;
 		};
 	};
-};	
-			
-			
-			
-			};
-			search(this.root, new String());
-			return words.length > 0 ? words : null;
+	search(this.root, new String());
+	return words.length > 0 ? words : null;
 	};		
 };
 
@@ -58,4 +68,4 @@ myTrie.add('sense');
 console.log(myTrie.IsWord('doll'))
 console.log(myTrie.IsWord('dor'))
 console.log(myTrie.IsWord('dorf'))
-console.log(myTrie.print())
+console.log(myTrie.print()) 
