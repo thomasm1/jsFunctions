734ae36d29150441bcc9e4ac26ab13f6f52b9e39
Thomas M. Maestas <thomasm1@users.noreply.github.com>
2017-11-19 19:48:18 -0700

    Add files via upload

---
+++ ReactJS/src/Components/Projects.js
@@ -0,0 +1,24 @@
+import React, { Component } from 'react';
+import ProjectItem from './ProjectItem';
+ 
+class Projects extends Component { 
+  render() {
+      let projectItems;
+      if(this.props.projects){
+        projectItems = this.props.projects.map(project => {
+
+   return (       
+    <ProjectItem key={project.title} project={project} /> 
+          ); 
+     });
+  } 
+    return (
+      <div className="Projects"> 
+       <h4>Projects</h4>
+       {projectItems}            
+       </div>
+    );
+  }
+}
+
+export default Projects;
