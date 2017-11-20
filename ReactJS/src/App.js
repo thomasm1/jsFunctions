9c5294984bb4bee571630057ec7ed72895425e9f
Thomas M. Maestas <thomasm1@users.noreply.github.com>
2017-11-20 15:02:32 -0800

    condense

--- ReactJS/src/App.js
+++ ReactJS/src/App.js
@@ -2,8 +2,7 @@ import React, { Component } from 'react';
 import Projects from './Components/Projects';
 import AddProject from './Components/AddProject';
 import logo from './logo.svg';
-import './App.css';
- 
+import './App.css'; 
 
 class App extends Component { 
     constructor() { 
@@ -13,44 +12,41 @@ class App extends Component {
      }
     }
 
-
-    componentWillMount(){   // this is life-cycle method
-this.setState({projects: [  
-  {
-        title: 'Xpansiv1 ', 
-        category: 'Page One'
-       },{
-        title: 'Xpansiv2',
-        category: 'Page Two'
-       },{
-        title: 'Xpansiv3 ', 
-        category: 'Page Three'
-       },{
-        title: 'Xpansiv4', 
-        category: 'Page Four'
-  }
-       ]
-});
+	componentWillMount(){   // this is life-cycle method
+		this.setState({projects: [  
+	  {
+		title: ' title 1 ', 
+		category: 'Page One'
+	       },{
+		title: ' title  2 ',
+		category: 'Page Two'
+	       },{
+		title: ' title  3 ', 
+		category: 'Page Three'
+	       },{
+		title: ' title  4', 
+		category: 'Page Four'
+	  }
+	       ]
+	});
     }
-    handleAddProject(project){
-      console.log(project);
-      let projects = this.state.projects;
-      projects.push(project);
-      this.setState({projects:projects});
+    	handleAddProject(project){
+	      console.log(project);
+	      let projects = this.state.projects;
+	      projects.push(project);
+	      this.setState({projects:projects});
     }
 
-    render() { 
-    return (
-      <div className="App">
-      <AddProject addProject={this.handleAddProject.bind(this)} />
-      <Projects projects={this.state.projects} />
-        <div className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">XPANSIV REACT - 1 </h1>
-	      </div >
-       
-        
-      </div>
+	render() { 
+	return (
+	      <div className="App">
+	      <AddProject addProject={this.handleAddProject.bind(this)} />
+	      <Projects projects={this.state.projects} />
+		<div className="App-header">
+		  <img src={logo} className="App-logo" alt="logo" />
+		  <h1 className="App-title">REACT PROJECT- 1 </h1>
+		</div >
+	      </div>
     );
   }
 }
