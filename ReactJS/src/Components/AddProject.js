7d08ddd4476e32c409fd4a5efb5d1fcb139b870e
Thomas M. Maestas <thomasm1@users.noreply.github.com>
2017-11-20 22:37:44 -0700

    Update AddProject.js

--- ReactJS/src/Components/AddProject.js
+++ ReactJS/src/Components/AddProject.js
@@ -1,4 +1,5 @@
-import React, { Component } from 'react'; 
+import React, { Component } from 'react';  
+import uuid from 'uuid';
 
 class AddProject extends Component { 
   constructor(){
@@ -16,6 +17,7 @@ class AddProject extends Component {
     alert('Title is requreisd');
   } else {
     this.setState({newProject:{
+      id:uuid.v4(),
       title: this.refs.title.value,
       category: this.refs.category.value
     }}, function(){
