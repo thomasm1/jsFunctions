3b3ec1c2db5b9b7a5a7660914f35d49a94be24f7
Thomas M. Maestas <thomasm1@users.noreply.github.com>
2017-11-20 22:39:47 -0700

    Update Projects.js

--- ReactJS/src/Components/Projects.js
+++ ReactJS/src/Components/Projects.js
@@ -2,13 +2,16 @@ import React, { Component } from 'react';
 import ProjectItem from './ProjectItem';
  
 class Projects extends Component { 
+  deleteProject(id){
+    this.props.onDelete(id);
+  }
   render() {
       let projectItems;
       if(this.props.projects){
         projectItems = this.props.projects.map(project => {
 
    return (       
-    <ProjectItem key={project.title} project={project} /> 
+    <ProjectItem onDelete={this.deleteProject.bind(this)} key={project.title} project = {project} /> 
           ); 
      });
   } 
