864481aadf1d53a321030237258770384d0a2f08
Thomas M. Maestas <thomasm1@users.noreply.github.com>
2017-11-21 23:32:30 -0700

    Create mod.js

---
+++ webpack/src/js/mod.js
@@ -0,0 +1,17 @@
+let mod = [
+{datum: 'data piece one'},
+{datum: 'data piece two'},
+{datum: 'data piece three'}];
+module.exports = mod;
+
+// module.exports = getResponse();
+
+
+/*
+function getResponse(){
+	return 'X0';
+};
+ module.exports = Ex-PeNs-iVe--November 21';
+ */
+
+
