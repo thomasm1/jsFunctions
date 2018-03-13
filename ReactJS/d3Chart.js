745779bb6f37f64e4a0aaf6fd9944389190c8391
Thomas M. Maestas <thomasm1@users.noreply.github.com>
2018-03-13 00:06:59 -0600

    Create d3Chart.js

---
+++ ReactJS/d3Chart.js
@@ -0,0 +1,20 @@
+let obj = data2.shift(),
+  formattedData = {labels: [obj.name], datasets:[]},
+  map = new Map();
+
+delete obj.name;
+for(var item in obj){
+  console.log(item, obj[item]);
+  }
+ 
+ const data = {
+  type:"bar",
+  data:{
+    labels["CAT1", "CAT2", "CAT3", "CAT4", "CAT5"],
+    datasets:[{
+      label:'Low',
+      data:[23,23,25, 18, 29],
+      backgroundColor:"rgba(255,0,0,.2)"
+      }]
+      }
+      };
