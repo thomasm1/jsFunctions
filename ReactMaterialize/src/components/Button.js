86c0e078e25a43cbe4d2d7331edde4afbee83b09
Thomas M. Maestas <thomasm1@users.noreply.github.com>
2018-03-15 18:24:39 -0600

    Create Button.js

---
+++ ReactMaterialize/src/components/Button.js
@@ -0,0 +1,8 @@
+
+        import {Button, Icon} from 'react-materialize'
+
+        export default () => (
+          <Button waves='light'>
+            <Icon>thumb_up</Icon>
+          </Button>
+        )
