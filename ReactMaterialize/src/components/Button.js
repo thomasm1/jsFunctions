c484b66616636c7c9467bab6e26b45c1ae43f164
Thomas Maestas <tmaestas@xpansivdata.com>
2018-05-02 03:13:40 -0600

    deleted:    ReactJS/src/AppApi.js 	deleted:    reactFunctional/TomsTodoList/package.json 	deleted:    webpack/dist/bundle.js 	deleted:    webpack/package.json 	deleted:    webpack/src/css/style.css

--- ReactMaterialize/src/components/Button.js
+++
@@ -1,8 +0,0 @@
-
-        import {Button, Icon} from 'react-materialize'
-
-        export default () => (
-          <Button waves='light'>
-            <Icon>thumb_up</Icon>
-          </Button>
-        )
