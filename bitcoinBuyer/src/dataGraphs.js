a68fe2074c4a5a61426300b6391d0431de2392bf
thomas maestas <thomasm1.maestas@gmail.com>
2019-07-13 12:25:51 -0600

    bitcoinBuyer, ArrayMaker: breadth-first Search

--- bitcoinBuyer/src/dataGraphs.js
+++ bitcoinBuyer/src/dataGraphs.js
@@ -1,5 +1,48 @@
 // Undirected & Directed Braphs
+// Breadth-first Search
 console.log(
+    "%c GRAPHS, Breadth-First Search: ",
+    "color:white; background-color:darkblue"
+  );
+  function breadthSearch(graph, root) {
+    var nodesLength = {};
+    for (var i = 0; i < graph.length; i++) {
+      nodesLength[i] = Infinity;
+    }
+    nodesLength[root] = 0;
+  
+    var queue = [root];
+    var current;
+  
+    while (queue.length !== 0) {
+      current = queue.shift();
+  
+      var curConnected = graph[current];
+      var neighborIdx = [];
+      var idx = curConnected.indexOf(1);
+      while (idx !== -1) {
+        neighborIdx.push(idx);
+        idx = curConnected.indexOf(1, idx + 1);
+      }
+      for (var j = 0; j < neighborIdx.length; j++) {
+        if (nodesLength[neighborIdx[j]] === Infinity) {
+          nodesLength[neighborIdx[j]] = nodesLength[current] + 1;
+          queue.push(neighborIdx[j]);
+        }
+      }
+    }
+    return nodesLength;
+  }
+  var newBreadthGraph = [
+    [0, 1, 1, 1, 0],
+    [0, 0, 1, 0, 0],
+    [1, 1, 0, 0, 0],
+    [0, 0, 0, 1, 0],
+    [0, 1, 0, 0, 0]
+  ];
+  console.log(breadthSearch(newBreadthGraph, 1));
+  // Undirected & Directed Braphs
+  console.log(
     "%c GRAPHS, Adjacency Lists: ",
     "color:white; background-color:darkblue"
   );
