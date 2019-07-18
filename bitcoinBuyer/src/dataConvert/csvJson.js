d53ed7799b7a8f6a6ef50d938e5df8f16c4a4409
thomas maestas <thomasm1.maestas@gmail.com>
2019-07-18 10:17:18 -0600

    bitcoinBuyer: fixed converter with ',' to make array

--- bitcoinBuyer/src/dataConvert/csvJson.js
+++ bitcoinBuyer/src/dataConvert/csvJson.js
@@ -9,7 +9,7 @@ document.getElementById("dataConvert").innerHTML = `
     function render(data){
         d3.select(".dataConvert")
           .append("pre")
-          .text(JSON.stringify(data, null, 2));
+          .text(JSON.stringify(data, null, 2) + ',');
       }
       function type(d){
         d.Date = +d.Date;
@@ -21,3 +21,12 @@ document.getElementById("dataConvert").innerHTML = `
         return d;
       }
       d3.csv("data/Coinbase_BTCUSD_2019.csv", type, render);
+
+      /*
+function csv<object, string>(url: string, init: RequestInit, row: (rawRow: d3.DSVRowString<string>, index: number, columns: string[]) => object): Promise<d3.DSVParsedArray<object>> (+2 overloads)
+Fetches the CSV file at the specified input URL and returns a promise of an array of objects representing the parsed rows.
+The init object is passed along to the underlying call to fetch.
+The specified row conversion function is used to map and filter row objects to a more-specific representation; see dsv.csvParse for details.
+The first generic parameter describes the type of the object representation of a parsed row. 
+The second generic paremeter describes the column names as union of string literal types
+*/
