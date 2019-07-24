6ca538ae24d5688a08cb02dab376993847ca0420
thomas maestas <thomasm1.maestas@gmail.com>
2019-07-24 13:41:46 -0600

    BitcoinBuyer: add ether test endpoint

--- bitcoinBuyer/dataGetters/dataFetch.js
+++ bitcoinBuyer/dataGetters/dataFetch.js
@@ -1,60 +1,89 @@
-const postsAsync = [
+  
+  async function fetchAsyncEther() {
+    // const res = await fetch("https://jsonplaceholder.typicode.com/users");
+    //https://tmm-nov.s3.amazonaws.com/data/coins/ETHBTCUSD_2019.json
+    const res = await fetch(
+      "https://tmm-nov.s3.amazonaws.com/data/coins/Coinbase_ETHUSD_2019.json"
+    );
+    const data = await res.json();
+    document.getElementById("fetchAsyncEther").innerHTML =
+      "API ETHEREUM Fetched:<br /><br />https://tmm-nov.s3.amazonaws.com/data/coins/Coinbase_ETHUSD_2019.json<br /><br />Date:  " + data[5].Date + ' Symbol:  ' + data[5].Symbol + ' Close:  ' + data[5].Close;
+    console.log("async fetched ..", data);
+  }
+  fetchAsyncEther();
+
+  // ASYNC - AWAIT - FETCH
+  async function fetchAsync() {
+    // const res = await fetch("https://jsonplaceholder.typicode.com/users");
+    //https://tmm-nov.s3.amazonaws.com/data/coins/ETHBTCUSD_2019.json
+    const res = await fetch(
+      "https://tmm-nov.s3.amazonaws.com/data/coins/BTCUSD_2019.json"
+    );
+    const data = await res.json();
+    document.getElementById("asyncFetch").innerHTML =
+      "API Fetched:<br /><br />https://tmm-nov.s3.amazonaws.com/data/coins/BTCUSD_2019.json<br /><br />Date:  " + data[0].Date + ' Symbol:  ' + data[0].Symbol + ' Close:  ' + data[0].Close;
+    console.log("async fetched ..", data);
+  }
+  fetchAsync();
+
+
+const pricesAsync = [
   {
-    "Date":"2019-07-04",
-    "Symbol":"BTCUSD",
-    "Open":11976.42,
-    "High":12064.26,
-    "Low":11820,
-    "Close":11909.55
- },
- {
-    "Date":"2019-07-03",
-    "Symbol":"BTCUSD",
-    "Open":10829.18,
-    "High":12014.6,
-    "Low":10829.18,
-    "Close":11976.42
- },
- {
-    "Date":"2019-07-02",
-    "Symbol":"BTCUSD",
-    "Open":10577.63,
-    "High":10925,
-    "Low":9651,
-    "Close":10829.18
- },
- {
-    "Date":"2019-07-01",
-    "Symbol":"BTCUSD",
-    "Open":10761.26,
-    "High":11210.52,
-    "Low":9953,
-    "Close":10577.63 
- },
- {
-    "Date":"2019-06-30",
-    "Symbol":"BTCUSD",
-    "Open":11865.29,
-    "High":12200,
-    "Low":10650.06,
-    "Close":10761.26 
- }
+    "Date": "2019-07-04",
+    "Symbol": "BTCUSD",
+    "Open": 11976.42,
+    "High": 12064.26,
+    "Low": 11820,
+    "Close": 11909.55,
+    "Volume BTC": 1237.57,
+    "Volume USD": 14790355.69
+  },
+  {
+    "Date": "2019-07-03",
+    "Symbol": "BTCUSD",
+    "Open": 10829.18,
+    "High": 12014.6,
+    "Low": 10829.18,
+    "Close": 11976.42,
+    "Volume BTC": 36836.85,
+    "Volume USD": 419076229.34
+  },
+  {
+    "Date": "2019-07-04",
+    "Symbol": "ETHUSD",
+    "Open": 301.79,
+    "High": 304,
+    "Low": 300.39,
+    "Close": 301.5,
+    "Volume ETH": 3825.69,
+    "Volume USD": 1154988.12
+  },
+  {
+    "Date": "2019-07-03",
+    "Symbol": "ETHUSD",
+    "Open": 291.25,
+    "High": 303.83,
+    "Low": 289.61,
+    "Close": 301.79,
+    "Volume ETH": 105691.96,
+    "Volume USD": 31264397.55
+  },
   ];
   
-  function getPostsAsync() {
+  function getPricesAsync() {
     setTimeout(() => {
       let outputAsync = "";
-      postsAsync.forEach((postsAsync, index) => {
-        outputAsync += `<li>Date: ${postsAsync.Date} Symbol: ${postsAsync.Symbol}Close: ${postsAsync.Close}</li>`;
+      pricesAsync.forEach((pricesAsync, index) => {
+        outputAsync += `<li>Date:  ${pricesAsync.Date} Symbol:  ${pricesAsync.Symbol} Close:  ${pricesAsync.Close}</li>`;
       });
-      document.getElementById("asyncAwait").innerHTML = outputAsync;
+      document.getElementById("asyncAwait").innerHTML =  "Local combined Ether + Bitcoin:<br />" + outputAsync;
     }, 1000);
   }
   
-  function createPost(post) {
+  function addPrice(price) {
     return new Promise((resolve, reject) => {
       setTimeout(() => {
-        postsAsync.push(post);
+        pricesAsync.push(price);
         //const error = true;
         const error = false;
         if (!error) {
@@ -66,29 +95,24 @@ const postsAsync = [
     });
   }
   
-  // createPost({ title: "Promises post-three", body: "this new post3" })
-  //   .then(getPostsPromise)
+  // PricePrice({ title: "Promises price-three", body: "this new price3" })
+  //   .then(getPricesPromise)
   //   .catch(err => console.log(err));
   
   // ASYNC - AWAIT
   async function init() {
-    await createPost({ title: "ASYNC post-three", body: "this new post3" });
-    getPostsAsync();
+    await addPrice({  
+      "Date":"2019-06-30",
+    "Symbol":"BTCUSD",
+    "Open":11865.29,
+    "High":12200,
+    "Low":10650.06,
+    "Close":10761.26  });
+    getPricesAsync();
   }
   init();
   
-  // ASYNC - AWAIT - FETCH
-  async function fetchAsync() {
-    // const res = await fetch("https://jsonplaceholder.typicode.com/users");
-    const res = await fetch(
-      "https://tmm-nov.s3.amazonaws.com/data/coins/BTCUSD_2019.json"
-    );
-    const data = await res.json();
-    document.getElementById("asyncFetch").innerHTML =
-      "async Fetched:<br /><br />https://tmm-nov.s3.amazonaws.com/data/coins/BTCUSD_2019.json<br /><br />Date: " + data[0].Date + ' Symbol:' + data[0].Symbol + ' Close: ' + data[0].Close;
-    console.log("async fetched ..", data);
-  }
-  fetchAsync();
+
   // // Promise.all([p1,p2,p3,p4]).then(values => console.log(values)
   // const promise1 = Promise.resolve("prom 1");
   // const promise2 = 10;
--- bitcoinBuyer/dataGetters/dataPromise.js
+++ bitcoinBuyer/dataGetters/dataPromise.js
@@ -1,60 +1,80 @@
-const postsPromise = [
+const pricesPromise = [
   {
-    "Date":"2019-07-04",
-    "Symbol":"BTCUSD",
-    "Open":11976.42,
-    "High":12064.26,
-    "Low":11820,
-    "Close":11909.55
- },
- {
-    "Date":"2019-07-03",
-    "Symbol":"BTCUSD",
-    "Open":10829.18,
-    "High":12014.6,
-    "Low":10829.18,
-    "Close":11976.42
- },
- {
-    "Date":"2019-07-02",
-    "Symbol":"BTCUSD",
-    "Open":10577.63,
-    "High":10925,
-    "Low":9651,
-    "Close":10829.18
- },
- {
-    "Date":"2019-07-01",
-    "Symbol":"BTCUSD",
-    "Open":10761.26,
-    "High":11210.52,
-    "Low":9953,
-    "Close":10577.63 
- },
- {
-    "Date":"2019-06-30",
-    "Symbol":"BTCUSD",
-    "Open":11865.29,
-    "High":12200,
-    "Low":10650.06,
-    "Close":10761.26 
- }
+    "Date": "2019-07-04",
+    "Symbol": "BTCUSD",
+    "Open": 11976.42,
+    "High": 12064.26,
+    "Low": 11820,
+    "Close": 11909.55,
+    "Volume BTC": 1237.57,
+    "Volume USD": 14790355.69
+  },
+  {
+    "Date": "2019-07-03",
+    "Symbol": "BTCUSD",
+    "Open": 10829.18,
+    "High": 12014.6,
+    "Low": 10829.18,
+    "Close": 11976.42,
+    "Volume BTC": 36836.85,
+    "Volume USD": 419076229.34
+  },
+  {
+    "Date": "2019-07-02",
+    "Symbol": "BTCUSD",
+    "Open": 10577.63,
+    "High": 10925,
+    "Low": 9651,
+    "Close": 10829.18,
+    "Volume BTC": 41476.42,
+    "Volume USD": 424791613.92
+  },
+  {
+    "Date": "2019-07-01",
+    "Symbol": "BTCUSD",
+    "Open": 10761.26,
+    "High": 11210.52,
+    "Low": 9953,
+    "Close": 10577.63,
+    "Volume BTC": 37633.9,
+    "Volume USD": 396857365.17
+  },
+  {
+    "Date": "2019-07-04",
+    "Symbol": "ETHUSD",
+    "Open": 301.79,
+    "High": 304,
+    "Low": 300.39,
+    "Close": 301.5,
+    "Volume ETH": 3825.69,
+    "Volume USD": 1154988.12
+  },
+  {
+    "Date": "2019-07-03",
+    "Symbol": "ETHUSD",
+    "Open": 291.25,
+    "High": 303.83,
+    "Low": 289.61,
+    "Close": 301.79,
+    "Volume ETH": 105691.96,
+    "Volume USD": 31264397.55
+  },
   ];
   
-  function getPostsPromise() {
+  function getPricesPromise() {
     setTimeout(() => {
       let outputPromise = "";
-      postsPromise.forEach((postsPromise, index) => {
-        outputPromise += `<li>Date: ${postsPromise.Date} Symbol:  ${postsPromise.Symbol} Close: ${postsPromise.Close}</li>`;
+      pricesPromise.forEach((pricesPromise, index) => {
+        outputPromise += `<li>Date: ${pricesPromise.Date} Symbol:  ${pricesPromise.Symbol} Close: ${pricesPromise.Close}</li>`;
       });
-      document.getElementById("promises").innerHTML = outputPromise;
+      document.getElementById("promises").innerHTML = "Local combined Ether + Bitcoin:<br />" + outputPromise;
     }, 1000);
   }
   
-  function createPost(post) {
+  function addPrice(price) {
     return new Promise((resolve, reject) => {
       setTimeout(() => {
-        postsPromise.push(post);
+        pricesPromise.push(price);
         //const error = true;
         const error = false;
         if (!error) {
@@ -66,15 +86,22 @@ const postsPromise = [
     });
   }
   
-  createPost({ title: "Promises post-three", body: "this new post3" })
-    .then(getPostsPromise)
+  addPrice({
+    "Date":"2019-06-30",
+    "Symbol":"BTCUSD",
+    "Open":11865.29,
+    "High":12200,
+    "Low":10650.06,
+    "Close":10761.26 
+  })
+    .then(getPricesPromise)
     .catch(err => console.log(err));
   
   // Promise.all([p1,p2,p3,p4]).then(values => console.log(values)
-  const promise1 = Promise.resolve("prom 1");
+  const promise1 = Promise.resolve("Price 1");
   const promise2 = 10;
   const promise3 = new Promise((resolve, reject) =>
-    setTimeout(resolve, 2000, "Goodby!")
+    setTimeout(resolve, 2000, "Timeout-2000ms")
   );
   const promise4 = fetch(
     "https://tmm-nov.s3.amazonaws.com/data/coins/BTCUSD_2019.json"
@@ -87,7 +114,7 @@ const postsPromise = [
   Promise.all([promise1, promise2, promise3, promise4]).then(
     values =>
       (document.getElementById("promiseAll").innerHTML =
-        "promise All, Source:<br /> https://tmm-nov.s3.amazonaws.com/data/coins/BTCUSD_2019.json<br /><br /> Date: "+ values[3][1].Date + ' Symbol:'  + values[3][1].Symbol +  ' Close: ' + values[3][1].Close)
+        "API PromiseAll Source:<br /> https://tmm-nov.s3.amazonaws.com/data/coins/BTCUSD_2019.json<br /><br /> Date: "+ values[3][1].Date + ' Symbol:'  + values[3][1].Symbol +  ' Close: ' + values[3][1].Close)
     //typicode (document.getElementById("promiseAll").innerHTML = values[3][7].name)
   );
   
