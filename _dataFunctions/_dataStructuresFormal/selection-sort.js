2573099272fbaff7261e76185bb18ad644eed63f
thomas maestas <thomasm1.maestas@gmail.com>
2020-04-08 23:50:16 -0400

    selection-sort js

---
+++ _dataFunctions/_dataStructuresFormal/selection-sort.js
@@ -0,0 +1,83 @@
+// LEGACY VERSION (non ES2015 syntax)
+function sselectionSort(arr){
+    for(var i = 0; i < arr.length; i++){
+        var lowest = i;
+        for(var j = i+1; j < arr.length; j++){
+            if(arr[j] < arr[lowest]){
+                lowest = j;
+            }
+        }
+        if(i !== lowest){
+            //SWAP!
+            var temp = arr[i];
+            arr[i] = arr[lowest];
+            arr[lowest] = temp;
+        }
+    }
+    return arr;
+}
+
+// ES2015 VERSION
+function selectionSort(arr) {
+  const swap = (arr, idx1, idx2) =>
+    ([arr[idx1], arr[idx2]] = [arr[idx2], arr[idx1]]);
+
+  for (let i = 0; i < arr.length; i++) {
+    let lowest = i;
+    for (let j = i + 1; j < arr.length; j++) {
+      if (arr[lowest] > arr[j]) {
+        lowest = j;
+      }
+    }
+    if (i !== lowest) swap(arr, i, lowest);
+  }
+
+  return arr;
+}
+
+selectionSort([0,2,34,22,10,19,17]);
+
+////////// JAVA ///////////////
+// Java program for implementation of Selection Sort
+class SelectionSort
+{
+	void sort(int arr[])
+	{
+		int n = arr.length;
+
+		// One by one move boundary of unsorted subarray
+		for (int i = 0; i < n-1; i++)
+		{
+			// Find the minimum element in unsorted array
+			int min_idx = i;
+			for (int j = i+1; j < n; j++)
+				if (arr[j] < arr[min_idx])
+					min_idx = j;
+
+			// Swap the found minimum element with the first
+			// element
+			int temp = arr[min_idx];
+			arr[min_idx] = arr[i];
+			arr[i] = temp;
+		}
+	}
+
+	// Prints the array
+	void printArray(int arr[])
+	{
+		int n = arr.length;
+		for (int i=0; i<n; ++i)
+			System.out.print(arr[i]+" ");
+		System.out.println();
+	}
+
+	// Driver code to test above
+	public static void main(String args[])
+	{
+		SelectionSort ob = new SelectionSort();
+		int arr[] = {64,25,12,22,11};
+		ob.sort(arr);
+		System.out.println("Sorted array");
+		ob.printArray(arr);
+	}
+} 
