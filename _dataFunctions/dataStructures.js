6ccf73a78e5fef1c6fb3ad164ae54faea4630224
Thomas Maestas <thomasm1.maestas@gmail.com>
2022-09-09 22:47:01 -0400

    priorityArray/PriorityHeap;Stack classes

--- _dataFunctions/dataStructures.js
+++ _dataFunctions/dataStructures.js
@@ -1,14 +1,16 @@
  
-const StackObject = function () {
-    this.stackLen = 0;
+class StackObject  {
+   constructor() {
+	this.stackLen = 0;
     this.storage = {};
+   }
     // Adds a value onto end of stack  ... {2:"value2"}
-    this.push = function (value) {
+    push (value) {
         this.stackLen++;
         this.storage[this.stackLen] = value; // {1:"value1"}
     }
     // Removes and returns a value at the end of stack
-    this.pop = function () {
+    pop () {
         if (this.stackLen === 0) {
             return undefined;
         }
@@ -17,40 +19,67 @@ const StackObject = function () {
         this.stackLen--;
         return result;
     }
-    this.size = function () {
+   size () {
         return this.stackLen;
     }
     // Returns value at the end of the stack (BUT DOES NOT REMOVE ITEM)
-    this.peek = function () {
+    peek () {
         return this.storage[this.stackLen];
     }
 };
+let stackObj = new StackObject();
+stackObj.push("string in stack")
+stackObj.push([1,2,3,4])
+stackObj.push(7)
+console.log(stackObj.size()) 
+console.log(stackObj)
+console.log(stackObj.pop())
+console.log(stackObj.size())
 
-function QueueArray() {
-    collection = []; // collect items in queue
-    this.print = function () {
-        document.write(collection);
-    }; // helper function
-    this.enqueue = function (element) { // pushes first item on q
-        collection.push(element);
+class PriorityQueueArray { 
+	constructor(){
+		this.queue = []; // collect items in queue 
+	}
+    print () {   // document.write(queue);
+		console.log(this.queue)
+    };  
+    enqueue (element) { // pushes first item on q
+        this.queue.push(element);
     };
-    this.dequeue = function () { // takes item off queue
-        return collection.shift(); // pulls off first item in array , returns it
+	qPriority(val,priority) {
+		this.queue.push({ val, priority});
+		this.sort(); 
+	}
+	sort(){
+		this.queue.sort((a,b) => a.priority - b.priority)
+	}
+    dequeue () { // pulls off queue[0] in array , returns it
+        return this.queue.shift(); // 
     };
-    this.front = function () { // this returns item at 0 index, (no remove)
-        return collection[0];
+    front() { //   returns item queue[0], (no remove)
+        return this.queue[0];
     };
-    this.size = function () { // length method
-        return collection.length;
+    size()  {  
+        return this.queue.length;
     };
-    this.isEmpty = function () {
-        return (collection.length === 0);
+    isEmpty () {
+        return (this.queue.length === 0);
     };
 }
-
-
-
-// max heap
+let q = new PriorityQueueArray()
+q.enqueue("string in queue")
+q.enqueue([11,22,33,44])
+q.enqueue(77)
+q.qPriority("a",3)
+q.qPriority("b",2)
+q.enqueue([11,22,33,44])
+q.qPriority("c",33)
+q.qPriority("d",4)  
+console.log(q.size())
+console.log(q);
+console.log(q.dequeue())
+console.log(q.size())
+   
 class MaxBinaryHeap {
 	constructor() {
 		this.values = []
@@ -78,15 +107,13 @@ class MaxBinaryHeap {
 }
 let maxHeap = new MaxBinaryHeap()
  maxHeap.insert(12) 
- maxHeap.insert(2) 
- maxHeap.insert(112)  
- maxHeap.insert(133)  
+ maxHeap.insert(2)  
  maxHeap.insert(1233) 
  maxHeap.insert(222) 
  console.log(maxHeap.get())
  
  // priorityHeap
-class PriorityQueue {
+class PriorityQueueHeap {
 	constructor() {
 		this.values = [];
 	}
@@ -157,97 +184,96 @@ class PriorityNode {
 		this.priority = priority;
 	}
 }
-let pq = new PriorityQueue();
+let pq = new PriorityQueueHeap();
 pq.enqueue("low priority", 5)
 pq.enqueue("utmost", 1)
-pq.enqueue("emergency", 2)
 pq.enqueue("nonemergency", 3)
 pq.enqueue("less nonemergency", 4)
-console.log(pq.values)
+console.log(pq)
 
 
-//////////// TRIE   <===========Not returning corectly
-// trie data = structure  Prefix tree used to store associative structures
-class Node {
-	constructor() {
-		this.keys = new Map();
-		this.end = false;
-		this.setEnd = function() {
-			this.end = true;
-		}
-		this.isEnd = function() {
-			return this.end;
-		};
-	};
-}
+// //////////// TRIE   <===========Not returning correctly
+// // trie data = structure  Prefix tree used to store associative structures
+// class Node {
+// 	constructor() {
+// 		this.keys = new Map();
+// 		this.end = false;
+// 		this.setEnd = function() {
+// 			this.end = true;
+// 		}
+// 		this.isEnd = function() {
+// 			return this.end;
+// 		};
+// 	};
+// }
 
- class Trie  {
-	constructor() { 
-	this.root = new Node(); 
-	}
-	 add (inputArr, node = this.root) {  // if pass in node, it will use. --- If not, it will use this.root
-		if (inputArr.length == 0){
-			node.setEnd();
-			return;
-		} else if (!node.keys.has(inputArr[0])) {  // first letter not there; there are more letters, not yet at end of word
-			node.keys.set(inputArr[0], new Node());    // if not, here we'll set it with method input[0] first letter. 
-			return this.add(inputArr.substr(1), node.keys.get(inputArr[0])); // every letter after the first letter, than pass in node just created (node with letter 'B')
-		} else { 
-			return this.add(inputArr.substr(1), node.keys.get(inputArr[0]));  // if there was another of same letter (if already in try, not going start from [1]... rather new input substring)
-		};
-	};
-    isWord(word) {			// checking if word in.. (doesn't need to check every word, only first letter, so faster indexing)
-		let node = this.root; 		
-		while (word.length > 1) {    //////////// TRIE   <===========Not returning correctly
-			if (!node.keys.has(word[0]))  {
-				return false;
-			} else {
-				node = node.keys.get(word[0]);
-				word = word.substr(1);
-				return true;
-				};
-		};
-	}
- 	print() {   // helper function - create array of every word and searches 
-	let words = new Array();
-	let search = function(node, string) {
-		if (node.keys.size != 0) {
-			for (let letter of node.keys.keys()) {
-				search(node.keys.get(letter), string.concat(letter));
-			};
-			if (node.isEnd()) {
-				words.push(string);
-			};
-		} else {
-			string.length > 0 ? words.push(string) : undefined;
-			return;
-		};
-	};
-	search(this.root, new String());
-	return words.length > 0 ? words : null;
-	};		
-};  
+//  class Trie  {
+// 	constructor() { 
+// 	this.root = new Node(); 
+// 	}
+// 	 add (inputArr, node = this.root) {  // if pass in node, it will use. --- If not, it will use this.root
+// 		if (inputArr.length == 0){
+// 			node.setEnd();
+// 			return;
+// 		} else if (!node.keys.has(inputArr[0])) {  // first letter not there; there are more letters, not yet at end of word
+// 			node.keys.set(inputArr[0], new Node());    // if not, here we'll set it with method input[0] first letter. 
+// 			return this.add(inputArr.substr(1), node.keys.get(inputArr[0])); // every letter after the first letter, than pass in node just created (node with letter 'B')
+// 		} else { 
+// 			return this.add(inputArr.substr(1), node.keys.get(inputArr[0]));  // if there was another of same letter (if already in try, not going start from [1]... rather new input substring)
+// 		};
+// 	};
+//     isWord(word) {			// checking if word in.. (doesn't need to check every word, only first letter, so faster indexing)
+// 		let node = this.root; 		
+// 		while (word.length > 1) {    //////////// TRIE   <===========Not returning correctly
+// 			if (!node.keys.has(word[0]))  {
+// 				return false;
+// 			} else {
+// 				node = node.keys.get(word[0]);
+// 				word = word.substr(1);
+// 				return true;
+// 				};
+// 		};
+// 	}
+//  	print() {   // helper function - create array of every word and searches 
+// 	let words = new Array();
+// 	let search = function(node, string) {
+// 		if (node.keys.size != 0) {
+// 			for (let letter of node.keys.keys()) {
+// 				search(node.keys.get(letter), string.concat(letter));
+// 			};
+// 			if (node.isEnd()) {
+// 				words.push(string);
+// 			};
+// 		} else {
+// 			string.length > 0 ? words.push(string) : undefined;
+// 			return;
+// 		};
+// 	};
+// 	search(this.root, new String());
+// 	return words.length > 0 ? words : null;
+// 	};		
+// };  
 
-let myTrie = new Trie()
-myTrie.add('ball');
-myTrie.add('bat');
-myTrie.add('doll');
-myTrie.add('do');
-myTrie.add('dorm');
-myTrie.add('send');
-myTrie.add('sense'); 
-// myTrie.print()
-console.log(myTrie.isWord('xoll'));
-console.log(myTrie.isWord('doll'));
-console.log(myTrie.isWord('dor'));
-console.log(myTrie.isWord('brat'));
-console.log(myTrie.isWord('dorf'));
+// let myTrie = new Trie()
+// myTrie.add('ball');
+// myTrie.add('bat');
+// myTrie.add('doll');
+// myTrie.add('do');
+// myTrie.add('dorm');
+// myTrie.add('send');
+// myTrie.add('sense'); 
+// // myTrie.print()
+// console.log(myTrie.isWord('xoll'));
+// console.log(myTrie.isWord('doll'));
+// console.log(myTrie.isWord('dor'));
+// console.log(myTrie.isWord('brat'));
+// console.log(myTrie.isWord('dorf'));
 
 
-('sense');
-console.log(myTrie.IsWord('doll'));
-console.log(myTrie.IsWord('dor'));
-console.log(myTrie.IsWord('dorf'));('sense');
-console.log(myTrie.IsWord('doll'));
-console.log(myTrie.IsWord('dor'));
-console.log(myTrie.IsWord('dorf'));
+// ('sense');
+// console.log(myTrie.IsWord('doll'));
+// console.log(myTrie.IsWord('dor'));
+// console.log(myTrie.IsWord('dorf'));('sense');
+// console.log(myTrie.IsWord('doll'));
+// console.log(myTrie.IsWord('dor'));
+// console.log(myTrie.IsWord('dorf'));
